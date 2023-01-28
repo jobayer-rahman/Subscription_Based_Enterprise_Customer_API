216b1be94a5d7e67b80837707977325bf255a094
jobayer-rahman <jobayer_rahman@yahoo.com>
2023-01-29 01:18:04 +0600

    Added the testcase for validare_code fubctions

--- enterprise/tests.py
+++ enterprise/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .utils import validate_code
 
-# Create your tests here.
+from django.core.exceptions import ValidationError
+
+class ValidateCodeTest(TestCase):
+    # for utils.validate_code function
+    def test_validate_code(self):
+        # Test valid input
+        self.assertTrue(validate_code(11))
+
+        # Test input with length greater than 2
+        with self.assertRaises(ValidationError) as cm:
+            validate_code(123)
+        self.assertEqual(cm.exception.messages.pop(), '123 is more than two character.')
+
+        # Test input that does not start with 1
+        with self.assertRaises(ValidationError) as cm:
+            validate_code(21)
+        self.assertEqual(cm.exception.messages.pop(), "21 is not starting with 1.")
+
+
+
+
